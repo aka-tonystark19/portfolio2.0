3a19f0d08243f410a1b0515d80327c38ff8353c2
ericellb <ericellb@gmail.com>
2019-11-16 20:35:45 -0500

    Updates

---
+++ precache-manifest.3c7838ae78fe9fae7ee22174351fca4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ba298701a1cb769a1d32c8a9398bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac9ea85bfaeb69d7b192",
+    "url": "/static/css/main.9aab3391.chunk.css"
+  },
+  {
+    "revision": "eefa69eaa3cf4f0cf153",
+    "url": "/static/js/2.89c17215.chunk.js"
+  },
+  {
+    "revision": "ac9ea85bfaeb69d7b192",
+    "url": "/static/js/main.e65c5f17.chunk.js"
+  },
+  {
+    "revision": "cdb700d2e80b4e5fd39c",
+    "url": "/static/js/runtime-main.82a1467c.js"
+  }
+]);
